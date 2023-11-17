2e675412d225db574cb3d0beb256d231742a3e5f
oktma <martino@uia.no>
2023-11-17 14:32:37 +0100

    Updated Instructuions

---
+++ IceThimble_MicroROS/IceThimble_MicroROS.ino
@@ -0,0 +1,146 @@
+#include <micro_ros_arduino.h>
+#include <ESP32Servo.h>
+#include "TinyPICO.h"
+
+#include <stdio.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <std_msgs/msg/float32_multi_array.h>
+#include <geometry_msgs/msg/twist.h>
+
+rcl_subscription_t subscriber;
+geometry_msgs__msg__Twist msg;
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
+// Initial led object
+TinyPICO tp = TinyPICO();
+
+// Initialize servo code
+// Define struct containing force sensed data
+typedef struct force_data {
+  float force_sensed_index = 0;
+  float force_sensed_thumb = 0;
+  float force_sensed_middle = 0;
+  float force_sensed_ring = 0;
+  float force_sensed_little = 0;
+  float force_sensed_palm = 0;
+};
+
+// Initialize force_data struct
+struct force_data forceData;
+
+// Create servo objects
+Servo servo_index;
+Servo servo_thumb;
+Servo servo_middle;
+Servo servo_ring;
+//Servo servo_little;
+
+// Set servo pin number
+int servo_pin_thumb = 32;
+int servo_pin_index = 33;
+int servo_pin_middle = 27;
+int servo_pin_ring = 15;
+
+// Initialize other auxiliary variables
+int limit = 240;
+int offset[5] = {50, 70, 70, 70, 90}; // Offset values [thumb, index, middle, ring, little]
+
+void error_loop(){
+  while(1){
+    tp.DotStar_SetPixelColor(255, 0, 0 );
+    delay(100);
+  }
+}
+
+void ServoControl()
+{
+  servo_thumb.write(map(static_cast<int>(forceData.force_sensed_thumb), 0, 255, offset[0], limit));
+  servo_index.write(map(static_cast<int>(forceData.force_sensed_index), 0, 255, offset[1], limit));
+  servo_thumb.write(map(static_cast<int>(forceData.force_sensed_middle), 0, 255, offset[2], limit));
+  servo_index.write(map(static_cast<int>(forceData.force_sensed_ring), 0, 255, offset[3], limit));
+}
+
+void subscription_callback(const void *msgin)
+{  
+  const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
+  Serial.print("Message heard: ");
+  auto newPos = msg->linear;
+  auto newPos2 = msg->angular;
+  forceData.force_sensed_thumb = static_cast<float>(newPos.x);
+  forceData.force_sensed_index = static_cast<float>(newPos.y);
+  forceData.force_sensed_middle = static_cast<float>(newPos.z);
+  forceData.force_sensed_ring = static_cast<float>(newPos2.x);
+  
+  ServoControl();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  set_microros_wifi_transports("JM2_nett", "autyn33369", "192.168.0.107", 8888);
+
+  delay(2000);
+  
+  // Set Blu light for status
+  tp.DotStar_SetPixelColor(0, 0, 255);
+
+  allocator = rcl_get_default_allocator();
+
+  //create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "tinypico_finger_force", "", &support));
+
+  // create subscriber
+  RCCHECK(rclc_subscription_init_default(
+    &subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+    "finger_force"));
+
+  // create executor
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+
+  // Hardware setup
+  // Attach the servos to their respective pins
+  servo_index.attach(servo_pin_thumb); 
+  servo_thumb.attach(servo_pin_index);
+  servo_middle.attach(servo_pin_middle); 
+  servo_ring.attach(servo_pin_ring);
+
+  // Start up sequence
+  servo_index.write(0);
+  servo_thumb.write(0);
+  servo_middle.write(0);
+  servo_ring.write(0);
+  delay(500);
+  servo_index.write(offset[0]);
+  servo_thumb.write(offset[1]);
+  servo_middle.write(offset[2]);
+  servo_ring.write(offset[3]);
+  delay(500);
+  servo_index.write(0);
+  servo_thumb.write(0);
+  servo_middle.write(0);
+  servo_ring.write(0);
+  delay(2000);
+}
+
+void loop() {
+  // Status green for ready
+  tp.DotStar_SetPixelColor(0, 255, 0);
+  delay(10);
+  RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
+}
--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # IceThimble
-This repository includes the code used for our editied files for use of the [SimpleThimble Project](https://simplethimble.readthedocs.io/en/latest/index.html "Original project documentation").
 
-The updated code is modified to a Tiny Pico platform (using ESP32) for both hand device and  dongle. The esp32 servo library is required. 
+This repository includes the code used for our edited files for use of the [SimpleThimble Project](https://simplethimble.readthedocs.io/en/latest/index.html "Original project documentation").
 
-# Micro ROS version
+The updated code is modified to a Tiny Pico platform (using ESP32) for both hand device and dongle. 
+The esp32 servo library is required. 
+
+## Micro ROS version
 In adittion a new version using micro ros for arduino is added. A micro ros host need to be set up on the host computer and the communication os made on a topic ok the host. omputer. Due too difficulties with dynamic list, twist is used as message type. This is the connection between twist and fingers are
 
 linear:
-x - thunb
+x - thumb
 y - index
 z - middle
 
@@ -15,4 +17,72 @@ linear:
 x - ring
 y - little
 z - palm
+
+## How To Use
+
+To operate the IceThimble first clone the code in the IceThimble repository [IceThimble Github](https://github.com/Microttus/IceThimble/). 
+Open the code in a microcontroller IDE of choice. 
+Arduino official IDE is a perfectly fine choice and is available here: [Arduino IDE](https://www.arduino.cc/en/software).
+
+To be able to use an ESP bootloader,
+go to Board Manager and install the _esp32_ by Espressif. 
+Connect your TinyPICO board and in the top of the window, choose the active USB port 
+and select UM TinyPICO as a device type. 
+Before uploading the code, the correct Wi-Fi 
+settings should be set.
+
+```c++
+set_microros_wifi_transports("your_id", "your_pw", "host_ip", 8888);
+```
+
+In the void setup function, the code line  
+which has to be altered for your configurations. 
+Mark that these configurations should be written in as strings. 
+The first parameter is your network ID, 
+the second your network password, and the third is the IP address of the computer 
+which shall be used as host. 
+The last parameter sets the UDP port used for the communication. 
+Your network IP address can be found using the following command in a terminal. 
+Finally, upload the code to the microcontroller and unplug it.
+
+
+```text
+ip a
+```
+
+To proceed, a computer with Ubuntu 22 installed is necessary in addition to ROS Humble. A guide on how to install ROS can be found here \url{https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debians.html}. When ROS is installed, the MicroROS agent can be installed with snap using this command.
+
+```text
+sudo snap install micro-ros-agent
+```
+
+Both computer and microcontroller must be connected to the same network. 
+The network configuration should as mentioned above be set for the microcontroller. 
+Now launch the MicroROS agent with the following line. 
+The port number must be the same as in the configuration line in the Arduino code.
+
+```text
+micro-ros-agent udp4 -p 8888
+```
+
+The battery can now be connected to the servo hat connected to the microcontroller. 
+The status led should first light blue, and then the servos should do a startup sequence. 
+If no error is rained, the LED should turn green, indicating that it is connected and the main loop is spinning. 
+If a problem is detected, the LED indicator will turn red. 
+If the green light is turned on, some status messages in the terminal which the agent was launched should be visible. 
+Among other things, describing which nodes and topics is created.
+
+Values between 0 and 254 can now be posted on the created topics for control of the position of the thimble device. 
+The following line can be used to publish a single message on the topic.
+The finger devices should then alter its position accordingly.
+
+```text
+ros2 topic pub -once /finger_force geometry_msgs/msg/Twist "{linear: {x: 200.0, y: 200.0, z: 200.0}, angular: {x: 200.0, y: 200.0, z: 200.0}}"
+```
+
+
+
+```
+
+
 
